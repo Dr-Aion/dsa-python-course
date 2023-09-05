5b9371b5f508179e795722657d49c5348267e8bb
Dr-Aion <aiganym.moldagulova@gmail.com>
2023-09-05 23:46:55 +0400

    new array time/space complexity

--- Arrays/array_example1.py
+++ Arrays/array_example1.py
@@ -1,4 +1,21 @@
 import array as arr
-
+#Time Complexity: O(1)
+#Space Complexity: O(1)
 my_array = arr.array('i') #typecode: i - signed integer
 print(my_array)
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+my_array1 = arr.array('i', [1, 2, 4]) #typecode: i - signed integer
+print(my_array1)
+
+import numpy as np
+#Time Complexity: O(1)
+#Space Complexity: O(1)
+np_array = np.array([], dtype = int)
+print(np_array)
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+np_array1 = np.array([1, 2, 3, 4])
+print(np_array1)
+
+
