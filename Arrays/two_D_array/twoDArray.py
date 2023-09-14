54b8c7e53e12ae859ef5f9fd1551757d24540d1a
Dr-Aion <aiganym.moldagulova@gmail.com>
2023-09-14 23:39:44 +0400

    2Darray

--- Arrays/two_D_array/twoDArray.py
+++ Arrays/two_D_array/twoDArray.py
@@ -2,3 +2,13 @@
 #Day 2 - 10, 14, 11, 5
 #Day 3 - 12, 17, 12, 8
 #Day 4 - 15, 18, 14, 9
+
+import numpy as np
+
+twoDArray = np.array([[11, 15, 10, 6],
+                      [10, 14, 11, 5],
+                      [12, 17, 12, 8],
+                      [15, 18, 14, 9]])
+print(twoDArray)
+#Time complexity: O(M x N)
+#Space complexity: O(M x N)
