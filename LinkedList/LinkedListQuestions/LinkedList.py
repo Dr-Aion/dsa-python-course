c0000d8178b79c9adafac5fd8193ed18df6353d5
Dr-Aion <aiganym.moldagulova@gmail.com>
2023-11-13 23:39:54 +0400

    remove_duplicates() method

--- LinkedList/LinkedListQuestions/LinkedList.py
+++ LinkedList/LinkedListQuestions/LinkedList.py
@@ -45,6 +45,24 @@ class LinkedList:
         for i in range(n):
             self.add(random.randint(min_value, max_value))
         return self
+
+    def remove_duplicates(ll):
+        if ll.head is None:
+            return
+        current_node = ll.head
+        prev_node = None
+        while current_node:
+            runner = current_node
+            while runner.next:
+                if runner.next.value == current_node.value:
+                    runner.next = runner.next.next
+                else:
+                    runner = runner.next
+            prev_node = current_node
+            current_node = current_node.next
+ 
+        ll.tail = prev_node  
+        return ll.head
     
 customLinkedList = LinkedList()
 customLinkedList.generate(5, 1, 10)
