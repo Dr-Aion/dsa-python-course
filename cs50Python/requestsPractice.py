57f7c9f1f412804fa9830c5021b8332dcaf55c0a
Dr-Aion <aiganym.moldagulova@gmail.com>
2023-12-17 23:56:32 +0400

    python requests practice

--- cs50Python/requestsPractice.py
+++ cs50Python/requestsPractice.py
@@ -6,4 +6,7 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json()))
+# print(json.dumps(response.json(), indent=2))
+obj = response.json()
+for result in obj["results"]:
+    print(result["trackName"])
