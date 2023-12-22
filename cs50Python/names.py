f5940e94b207d526eabfce9045760e5487f887e2
Dr-Aion <aiganym.moldagulova@gmail.com>
2023-12-22 23:56:57 +0400

    csv practice

--- cs50Python/names.py
+++ cs50Python/names.py
@@ -10,5 +10,7 @@ with open("names.txt", "r") as file:
     for line in file:
         names.append(line.rstrip())
 
-for name in sorted(names):
+for name in sorted(names, reverse = True):
     print(f"hello, {name}")
+
+# csv - comma-seperated value
