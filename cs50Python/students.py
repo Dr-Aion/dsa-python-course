eb1692f8c2dcb8f9674a1709db9c6ff558a3f022
Dr-Aion <95173910+Dr-Aion@users.noreply.github.com>
2023-12-23 21:35:53 +0400

    Update students.py

--- cs50Python/students.py
+++ cs50Python/students.py
@@ -1,4 +1,8 @@
+students = [] 
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        students.append(f"{name} is in {house}")
+
+for student in sorted(students):
+    print student
