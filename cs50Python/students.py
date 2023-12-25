98f280ecaf7faaa3f980f0a83855ddb7c89ad1d6
Dr-Aion <aiganym.moldagulova@gmail.com>
2023-12-25 23:33:35 +0400

    lambda function

--- cs50Python/students.py
+++ cs50Python/students.py
@@ -4,6 +4,21 @@ students = []
 
 with open("students.csv") as file:
     for line in file:
-        man, home = line.rstrip().split(",")
-        student = {"names": name, "home":home}
+        # you can unpack the sequence
+        name, house = line.rstrip().split(",")
+        # print(f"{name} is in {house}")
+        # students.append(f"{name} is in {house}")
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        print(student)
         students.append(student)
+
+def get_name(student):
+    return student['name']
+
+def get_house(student):
+    return student['house']
+
+for student in sorted(students, key=get_house, reverse=True):
+    print(f"{student['name']} is in {student['house']}")
