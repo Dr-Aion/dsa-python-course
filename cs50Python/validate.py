31d00bc581a8d6e3765a5815117e3aa5cec45366
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-01-02 23:53:03 +0400

    validating an email using regular expressions

--- cs50Python/validate.py
+++ cs50Python/validate.py
@@ -1,7 +1,7 @@
 import re
 email = input("What's your email? ")
 
-if re.search(r"^\w+@\w+\.edu$", email, re.IGNORECASE):
+if re.search(r"^(\w|\.)+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
