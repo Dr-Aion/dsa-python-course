c93bd28e901233371fc75b7d8efa307d2504cc0a
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-01-03 23:57:37 +0400

    formatting the user input using regular expressions and a walrus operator

--- cs50Python/format.py
+++ cs50Python/format.py
@@ -1,9 +1,14 @@
 import re
 name = input("What's your name? ").strip()
 
+# 1st approach
 matches = re.search(r"^(.+), *(.+)$", name)
 if matches:
     # last = matches.group(1)
     # first = matches.group(2)
     name = matches.group(2) + " " + matches.group(1)
+
+#2nd approach using walrus operator
+# if matches := re.search(r"^(.+), *(.+)$", name): 
+#     name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
