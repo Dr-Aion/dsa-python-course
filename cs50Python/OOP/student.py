90b60787fd58afeb3289ddc1bd5baed7f3cdcc5b
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-01-06 15:47:04 +0400

    __str__ function

--- cs50Python/OOP/student.py
+++ cs50Python/OOP/student.py
@@ -1,19 +1,24 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
+    def __str__(self):
+        return f"{self.name} is from {self.house}"
     # attributes or instance variables: name, students
 
 def main():
     # get_student() returns a tuple
     student = get_student() 
-    print(f"{student.name} is from {student.house}")
+    print(student)
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    student = Student(name, house)
-    return student
+    name = input("Name: ").strip()
+    house = input("House: ").strip()
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
