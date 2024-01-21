5f042088dafabb5d01c94d03e15adb8d3ebb8032
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-01-21 23:55:36 +0400

    queue with no size limitations

--- Queue/QueueNoSize.py
+++ Queue/QueueNoSize.py
@@ -3,40 +3,46 @@ class Queue:
         self.items = []
     
     def __str__(self):
+        if self.isEmpty():
+            return "Queue is empty"
         values = [str(x) for x in self.items]
-        return ' '.join(values)
-    
+        return " ".join(values)
+
     def isEmpty(self):
-        if self.items == []:
+        if not self.items:
             return True
         else:
             return False
     
-    def enqueue(self, value):
+    # Time complexity: Amortized time O(1), in worst case scenario O(n) or even O(n^2)
+    def enqueue(self, value): 
         self.items.append(value)
-        return "The element is inserted at the end of Queue"
+        return "The element has been enqueued into the Queue"
     
     def dequeue(self):
         if self.isEmpty():
-            return "The is not any element in the Queue"
+            return "Queue is empty"
         else:
             return self.items.pop(0)
     
     def peek(self):
         if self.isEmpty():
-            return "The is not any element in the Queue"
+            return "Queue is empty"
         else:
             return self.items[0]
-    
+        
     def delete(self):
         self.items = None
-
-
-
+        
 
 customQueue = Queue()
+print(customQueue.isEmpty())
 customQueue.enqueue(1)
-customQueue.enqueue(2)
 customQueue.enqueue(3)
+customQueue.enqueue(5)
+print(customQueue)
+print(customQueue.dequeue())
+print(customQueue)
 print(customQueue.peek())
 customQueue.delete()
+print(customQueue)
