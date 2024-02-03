a43f030ceafc1783feb0f95cbcaa2a4e443026d4
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-02-03 23:51:28 +0400

    factorial

--- Recursions/Recursion.py
+++ Recursions/Recursion.py
@@ -6,3 +6,17 @@ def openRussianDoll(doll):
 
 
 openRussianDoll(4)
+
+
+ ## Factorial###
+
+
+def factorial(n):
+    assert n >= 0 and int(n) == n, 'The number must be positive integer only!'
+    if n in [0,1]:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+ #
