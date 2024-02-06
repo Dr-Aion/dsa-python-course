30f2ec0f9de4c5c587fb2ca81383dcfc53a2c01e
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-02-06 23:31:33 +0400

    fibonacci recursion

--- Recursions/Recursion.py
+++ Recursions/Recursion.py
@@ -74,5 +74,13 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
+## Fibonacci###
 
- #
+def fibonacci(n):
+    assert n >=0 and int(n) == n , 'Fibonacci number cannot be negative number or non integer.'
+    if n in [0,1]:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(7))
