76d8e0e43461149f9b2f6f9680aedbe29f03f905
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-02-22 23:52:57 +0400

    hello app changes

--- cs50WebDevelopmentWithPythonAndJavaScript/Django/djangoFirstProject/hello/views.py
+++ cs50WebDevelopmentWithPythonAndJavaScript/Django/djangoFirstProject/hello/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def aiganym(request):
     return HttpResponse("Hello, Aiganym!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
