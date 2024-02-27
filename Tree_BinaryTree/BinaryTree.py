abe8900a629697d817d3c82a3fe53384723bf372
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-02-27 23:56:19 +0400

    preOrder and inOrder Traversals

--- Tree_BinaryTree/BinaryTree.py
+++ Tree_BinaryTree/BinaryTree.py
@@ -15,3 +15,17 @@ leftChild.rightChild = coffee
 rightChild = TreeNode("Cold")
 newBT.leftChild = leftChild
 newBT.rightChild = rightChild
+
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
