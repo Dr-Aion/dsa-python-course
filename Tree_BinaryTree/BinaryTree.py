de52951aecf88680b7a1462da1ee72b7f6ee8960
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-02-28 23:29:26 +0400

    search binary tree

--- Tree_BinaryTree/BinaryTree.py
+++ Tree_BinaryTree/BinaryTree.py
@@ -51,3 +51,20 @@ def levelOrderTraversal(rootNode):
             
             if (root.value.rightChild is not None):
                 customQueue.enqueue(root.value.rightChild)
+
+def searchBT(rootNode, nodeValue):
+    if not rootNode:
+        return "The BT does not exist"
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            if root.value.data == nodeValue:
+                return "Success"
+            if (root.value.leftChild is not None):
+                customQueue.enqueue(root.value.leftChild)
+            
+            if (root.value.rightChild is not None):
+                customQueue.enqueue(root.value.rightChild)
+        return "Not found"
