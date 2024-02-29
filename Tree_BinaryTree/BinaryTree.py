51a7777d8d7963d5986a64d410ec4c237c8ea5d4
Dr-Aion <aiganym.moldagulova@gmail.com>
2024-02-29 23:50:39 +0400

    insert a node to BT

--- Tree_BinaryTree/BinaryTree.py
+++ Tree_BinaryTree/BinaryTree.py
@@ -68,3 +68,22 @@ def searchBT(rootNode, nodeValue):
             if (root.value.rightChild is not None):
                 customQueue.enqueue(root.value.rightChild)
         return "Not found"
+
+def insertNodeBT(rootNode, newNode):
+    if not rootNode:
+        rootNode = newNode
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            else:
+                root.value.leftChild = newNode
+                return "Successfully Inserted"
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+            else:
+                root.value.rightChild = newNode
+                return "Successfully Inserted"
