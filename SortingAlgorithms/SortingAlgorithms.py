8d0e7498cc509b56409fa1516e97207f3990d107
Dr-Aion <95173910+Dr-Aion@users.noreply.github.com>
2025-06-17 13:46:16 +0400

    MergeSort fixed

--- SortingAlgorithms/SortingAlgorithms.py
+++ SortingAlgorithms/SortingAlgorithms.py
@@ -52,5 +52,50 @@ def bucketSort(customList):
             k += 1
     return (customList)
 
-print(bucketSort(customList))
-bucketSort(customList)
+# print(bucketSort(customList))
+# bucketSort(customList)
+
+def merge(customList, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    for i in range(0, n1):
+        L[i] = customList[l + i]
+    for j in range(0, n2):
+        R[j] = customList[m + 1 + j]
+    
+    i = 0
+    j = 0
+    k = l
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            customList[k] = L[i]
+            i += 1
+        else:
+            customList[k] = R[j]
+            j += 1
+        k += 1
+    while i < n1:
+        customList[k] = L[i]
+        i += 1
+        k += 1
+    while j < n2:
+        customList[k] = R[j]
+        j += 1
+        k += 1
+    return (customList)
+
+def mergeSort(customList, l, r):
+    if l < r:
+        m = (l + r)//2
+        mergeSort(customList, l, m)
+        mergeSort(customList, m + 1, r)
+        merge(customList, l, m, r)
+    return (customList)
+
+customList = [5, 7, 4, 3, 8, 6, 1, 9, 2]
+print(mergeSort(customList, 0, 8))
+        
